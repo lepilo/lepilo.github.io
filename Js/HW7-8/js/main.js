789d2a9650652391c35170e768ea1c15e59205d3
Denis Roschin <magicpsychill@icloud.com>
2016-07-26 13:51:41 +0300

    HW7-8 edit

--- Js/HW7-8/js/main.js
+++ Js/HW7-8/js/main.js
@@ -5,18 +5,25 @@ $(function() {
 		$('#tabs ' + $currentAttr).fadeIn(1000).siblings('div').removeClass('active').hide();
 	});
 
-	$('#button').on('click',function(e) {
-		e.preventDefault();
-		$('.help').toggleClass('show');
-	})
-	$('.string').hover(
-		function() {
-			$(this).find('.help').addClass('show');
-		},
-		function() {
-			$(this).find('.help').removeClass('show');
+var $title;
+
+
+	$('.inp').hover(
+		function(){
+			$title = $(this).attr('title');
+			console.log($title);
+			$(this).removeAttr('title').siblings('.help').show(100).html($title);
+		}, 	
+		function(){
+			$(this).siblings('.help').html('')
+			$(this).attr('title', $title);
 		});
-
+	$('button').on('click', function() {
+			for(var i=1; i < 4; i++){
+				var title = $('.string'+i).find('.inp').attr('title');
+				console.log(title);
+				var span = $('.string'+i).find('.help').show(100).html(title).delay(2000).fadeOut(300);
+				console.log(span);
+			}
+	})
 });
-
-
